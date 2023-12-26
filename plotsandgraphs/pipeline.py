59aa72b039ff480896067946bf6ac1d6f695f362
Joshua Wendland <80349780+joshuawe@users.noreply.github.com>
2023-12-26 13:25:33 +0100

    add pipeline notebook

--- plotsandgraphs/pipeline.py
+++ plotsandgraphs/pipeline.py
@@ -7,7 +7,7 @@ from tqdm.auto import tqdm
 def binary_classifier(y_true, y_score, save_fig_path=None, plot_kwargs={}):
     
     # Create new tqdm instance
-    tqdm_instance = tqdm(total=7, desc='Binary classifier metrics')
+    tqdm_instance = tqdm(total=6, desc='Binary classifier metrics', leave=True)
     
     # Update tqdm instance
     tqdm_instance.update()
@@ -15,25 +15,31 @@ def binary_classifier(y_true, y_score, save_fig_path=None, plot_kwargs={}):
     # 1) Plot ROC curve
     roc_kwargs = plot_kwargs.get('roc', {})
     bc.plot_roc_curve(y_true, y_score, save_fig_path=save_fig_path, **roc_kwargs)
+    tqdm_instance.update()
     
     # 2) Plot precision-recall curve
     pr_kwargs = plot_kwargs.get('pr', {})
     bc.plot_pr_curve(y_true, y_score, save_fig_path=save_fig_path, **pr_kwargs)
+    tqdm_instance.update()
     
     # 3) Plot calibration curve
     cal_kwargs = plot_kwargs.get('cal', {})
     bc.plot_calibration_curve(y_true, y_score, save_fig_path=save_fig_path, **cal_kwargs)
+    tqdm_instance.update()
     
     # 3) Plot confusion matrix
     cm_kwargs = plot_kwargs.get('cm', {})
     bc.plot_confusion_matrix(y_true, y_score, save_fig_path=save_fig_path, **cm_kwargs)
+    tqdm_instance.update()
     
     # 5) Plot classification report
     cr_kwargs = plot_kwargs.get('cr', {})
     bc.plot_classification_report(y_true, y_score, save_fig_path=save_fig_path, **cr_kwargs)
+    tqdm_instance.update()
     
     # 6) Plot y_score histogram
     hist_kwargs = plot_kwargs.get('hist', {})
-    bc.plot_y_prob_histogram(y_true, y_score, save_fig_path=save_fig_path, **hist_kwargs)
+    bc.plot_y_score_histogram(y_true, y_score, save_fig_path=save_fig_path, **hist_kwargs)
+    tqdm_instance.update()
     
     return
